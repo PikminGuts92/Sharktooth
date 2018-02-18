b0f8473eded31dca32bf69d7838ed9db5b6d0121
PikminGuts92 <PikminGuts92@gmail.com>
2018-02-18 01:00:46 -0500

    Added xmk converter project

---
+++ Xmk2Mid/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sharktooth;
+
+namespace Xmk2Mid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XMK xmk = XMK.FromFile(args[0]);
+            XMKExport mid = new XMKExport(xmk);
+            mid.Export(args[1]);
+        }
+    }
+}
