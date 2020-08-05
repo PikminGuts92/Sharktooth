901497d73faea7cdd56734e409b363c84d563186
PikminGuts92 <PikminGuts92@gmail.com>
2020-08-04 22:45:02 -0400

    Added log output to script

--- build.sh
+++ build.sh
@@ -4,19 +4,24 @@ OUTPUT_PATH="./BUILD"
 ZIP_PATH="$OUTPUT_PATH/Sharktooth-$BUILD_VERSION.zip"
 
 # Clear previous build
+echo ">> Clearing old files"
 rm $OUTPUT_PATH -rf
 
 # Build + publish projects
+echo ">> Building solution"
 dotnet publish -c $BUILD_MODE -o $OUTPUT_PATH -p:Version=$BUILD_VERSION
 
 # Delete debug + config files
+echo ">> Removing debug files"
 rm ./$OUTPUT_PATH/*.config -f
 rm ./$OUTPUT_PATH/*.pdb -f
 
 # Copy licences + README
+echo ">> Copying licenses and README"
 cp ./LICENSE ./$OUTPUT_PATH/LICENSE -f
 cp ./THIRDPARTY ./$OUTPUT_PATH/THIRDPARTY -f
 cp ./README.md ./$OUTPUT_PATH/README.md -f
 
 # Zip everything up
+echo ">> Zipping everything up"
 zip $ZIP_PATH $OUTPUT_PATH -jr
